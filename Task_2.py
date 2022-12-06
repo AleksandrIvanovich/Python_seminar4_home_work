769eb181751978004096b52b442b59835e695477
Aleksandr <fomichov73@mail.ru>
2022-12-06 22:47:06 +0300

    Added solution Task 2

--- Task_2.py
+++ Task_2.py
@@ -2,20 +2,29 @@
 # неповторяющихся элементов исходной последовательности. Не использовать множества.
 # Постарайтесь решить за одно условие
 # [1,1,1,1,2,2,2,3,3,3,4] -> [1,2,3,4]
+import random
 
 def give_int(input_string:str):
     while True:
         try:
             num = int(input(input_string))
             if num < 0:
-                num = abs(num)
+                 num = abs(num)
             return num
         except:
             print('Попробуйте еще раз. Вы ввели не число')
 
-def random_list (length_list, down_border, up_border):
-   
-    rand_list=[]
-    for i in range(0,length_list):
-        rand_list.append(random.randint(down_border,up_border))
-    return rand_list
+num_len = give_int("Введите число (количество элементов в списке):\n")           
+num_max = give_int("Введите число (максимальный элемент в списке):\n") 
+
+rand_list = []
+for i in range(0,num_len):
+        rand_list.append(random.randint(1,num_max))
+        
+result_list = []
+for i in rand_list:
+     if i not in result_list:
+          result_list.append(i)
+    
+print(rand_list)
+print(result_list)
